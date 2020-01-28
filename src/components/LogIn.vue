a6e2868b5dfae74bba255f221b9506cb1f90dfd9
Andrey <36811054+fasagod@users.noreply.github.com>
2020-01-28 23:46:01 +0400

    +login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,10 +1,72 @@
 <template>
-    
+  <div>
+    <el-form
+      :model="loginForm"
+      status-icon
+      :rules="rules"
+      ref="loginForm"
+      label-width="120px"
+      class="loginForm"
+    >
+      <el-form-item label="Username" prop="username">
+        <el-input type="username" v-model="loginForm.username" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="Password" prop="pass">
+        <el-input type="password" v-model="loginForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('loginForm')">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+
 export default Vue.extend({
-    
-})
+  data() {
+      const validateUsername = (rule: any, value: any, callback: any) => {
+        if (value === '') {
+          callback(new Error('Please input the username'));
+        } else {
+          callback();
+        }
+      };
+      const validatePass = (rule: any, value: any, callback: any) => {
+        if (value === '') {
+          callback(new Error('Please input the password again'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        labelPosition: 'right',
+        loginForm: {
+          username: '',
+          pass: '',
+        },
+        rules: {
+          username: [
+            { validator: validateUsername, trigger: 'blur' },
+          ],
+          checkPass: [
+            { validator: validatePass, trigger: 'blur' },
+          ],
+        },
+      };
+    },
+    methods: {
+      submitForm(formName: string) {
+        (this.$refs[formName]as any).validate((valid: any) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            alert('error submit!!');
+            return false;
+          }
+        });
+      },
+    },
+});
 </script>
